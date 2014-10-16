5b5b9e3f5a3e41e890caab3d3912b240facf89a9
Richard Ashley <rashley@gmail.com>
2014-10-16 20:34:10 +0100

    Adding findLTObject

--- findLTObject.py
+++ findLTObject.py
@@ -28,9 +28,6 @@ class wcssolution:
 		for o in self.objects:
 			catRA = astropy.coordinates.angles.Angle(o['field_ra'], 'degree')
 			catDEC= astropy.coordinates.angles.Angle(o['field_dec'], 'degree')
-			#raDistance = catRA - targetRADEC[0]
-			#decDistance = catDEC - targetRADEC[1]
-			#totalDistance = math.sqrt( raDistance.degree * raDistance.degree + decDistance.degree*decDistance.degree)
 			c1 = astropy.coordinates.ICRS(catRA.degree, catDEC.degree, unit=(u.degree, u.degree))
 			c2 = astropy.coordinates.ICRS(targetRADEC[0].degree, targetRADEC[1].degree, unit=(u.degree, u.degree))
 			separation = c1.separation(c2)
@@ -41,7 +38,6 @@ class wcssolution:
 			if totalDistance<closestDistance:
 				closestDistance = totalDistance
 				matchedObject = o
-		#print matchedObject, "at a distance of", closestDistance
 		return matchedObject, closestDistance
 
 def readCSVCatalog(filename):
@@ -56,12 +52,25 @@ def readCSVCatalog(filename):
 		object['ra'] = astropy.coordinates.angles.Angle(line[1], 'hour')
 		object['dec'] = astropy.coordinates.angles.Angle(line[2], 'degree')
 		coords = [ line[1], line[2] ]
-		#c = SkyCoord(coords, FK4, unit=(u.deg, u.hourangle), obstime="J1992.21")
 		objectList.append(object)
 	
 	return objectList
-	
 
+
+
+def getRefCoords(filename):	
+	wcsFile = astropy.io.fits.open(filename)
+	header = wcsFile[0].header
+		
+	equinox = float(header['EQUINOX'])
+	referenceCoord = astropy.coordinates.angles.Angle(float(header['CRVAL1']), 'degree'), astropy.coordinates.angles.Angle(float(header['CRVAL2']), 'degree')
+	referencePixel = float(header['CRPIX1']), float(header['CRPIX2'])
+	
+	return (referenceCoord)		
+	
+def loadOriginalFITSheaders(filename):
+	inputFile = astropy.io.fits.open(filename)
+	
 def loadWCSsolution(filename):
 	solution = wcssolution(filename)
 	
@@ -84,9 +93,9 @@ if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description='Find LT object. Looks in a CSV catalog for an object and matches it to an object in an LT image.')
 	parser.add_argument('csvfile', type=str, help='Input catalog in CSV format')
+	parser.add_argument('-n', '--nomatch', action='store_true', help='Suppress reporting of non-matches')
 	
 	arg = parser.parse_args()
-	print arg
 	
 	objectList = readCSVCatalog(arg.csvfile)
 	
@@ -94,32 +103,54 @@ if __name__ == "__main__":
 	
 	wcsSolutions = []
 	for f in fileList:
-		print "Loading Astrometry.net solution file: ", f
+		print "Loading Astrometry.net solution file: ", f, 
 		wcs_solution = loadWCSsolution(f)
-		wcsSolutions.append(wcs_solution)	
-	print "Loaded a total of:", len(wcsSolutions), " wcs solutions."
+		wcsSolutions.append(wcs_solution)
+		# Also load the WCS FITS headers to get a coord somewhere in the field
+		wcsFilename = f[:-4] + "wcs"
+		refCoords = getRefCoords(wcsFilename)
+		print "... reference coord is:", refCoords[0].to_string(unit=u.hour, sep=':'), refCoords[1].to_string(unit=u.degree, sep=':')
+	print "Loaded a total of ", len(wcsSolutions), " wcs solutions."
 	
-	for target in objectList:
-	
-		print "Looking for a match for:", target['name'], " { RA:", target['ra'], "DEC:", target['dec'], "}"
-	
-		
-		
-		closestDistance = 360
+	#for i in range(len(objectList)):
+	for i in range(70,71):
+		target = objectList[i]
+		#print "Looking for a match for [%d]:"%i, target['name'], " { RA:", target['ra'], "DEC:", target['dec'], "}"
+		matches = []
 		for w in wcsSolutions:
-			match, distance = w.findNearestObject((target['ra'], target['dec']))
-			if (distance<closestDistance):
-				closestDistance = distance
-				matchedObject = match
-				matchedField = w.filename[:-4] + "fits"
+			fieldmatch, distance = w.findNearestObject((target['ra'], target['dec']))
+			if distance<(0.16): 
+				match = {}
+				#print "Found in field", distance, w.filename
+				match['object'] = fieldmatch
+				match['field'] = w
+				match['distance'] = distance
+				matches.append(match)
 		
-		if closestDistance > 0.16:
-			print "    No match: (closest was %3.2f degrees away)"%(closestDistance)
-		else:
-			print "    Found a match in file: ", matchedField, " Object with pixel coordinates of (%3.2f, %3.2f)"%(matchedObject['field_x'], matchedObject['field_y']),
-			print "is %3.2f arcseconds away"%(closestDistance * 60 * 60)
+		if len(matches)>0:
+			print "For:", target['name'], "[%d]"%i
 			
-		print
+			for m in matches:						
+				matchFilename = m['field'].filename
+				matchedObject = m['object']
+				distance = m['distance']
+				print "  Found a match in file: ", matchFilename
+				print "    Object with pixel coordinates of (%3.2f, %3.2f)"%(matchedObject['field_x'], matchedObject['field_y']),
+				print "is %3.2f arcseconds away"%(distance * 60 * 60)
+				field_ra = astropy.coordinates.angles.Angle(matchedObject['field_ra'], 'degree')
+				field_dec = astropy.coordinates.angles.Angle(matchedObject['field_dec'], 'degree')
+				index_ra = astropy.coordinates.angles.Angle(matchedObject['index_ra'], 'degree')
+				index_dec = astropy.coordinates.angles.Angle(matchedObject['index_dec'], 'degree')
+				print "    Position given in the csv file :", target['ra'].to_string(unit=u.hour, sep=':'), target['dec'].to_string(unit=u.degree, sep=':')
+				print "    Catalog position               :", index_ra.to_string(unit=u.hour, sep=':'), index_dec.to_string(unit=u.degree, sep=':')
+				print "    Astrometry coomputed position  :", field_ra.to_string(unit=u.hour, sep=':'), field_dec.to_string(unit=u.degree, sep=':')
+				c1 = astropy.coordinates.ICRS(index_ra.degree, index_dec.degree, unit=(u.degree, u.degree))
+				c2 = astropy.coordinates.ICRS(field_ra.degree, field_dec.degree, unit=(u.degree, u.degree))
+				separation = c1.separation(c2)
+				print "    Separation between catalog and astrometry computed position:", separation.arcsecond, " arcseconds"
+				print 
+				
+			
 		
 				
 	
