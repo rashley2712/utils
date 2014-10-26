4a2b6ac2b223cf7855d534b0b9f3b36afcb295cc
Richard Ashley <rashley@gmail.com>
2014-10-26 21:27:08 +0100

    Trying photutils

--- astropyTest.py
+++ astropyTest.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 import astropy 
 import sys
+import photutils
+import numpy as np
+from photutils import datasets
 
+print "Python version:", sys.version
 print "Astropy version:", astropy.__version__
 
-print astropy
+hdu = datasets.load_star_image()   
+image = hdu.data[500:700, 500:700]   
+image -= np.median(image)
 
-print sys.version
+from photutils import daofind
+from astropy.stats import median_absolute_deviation as mad
+bkg_sigma = 1.48 * mad(image)   
+sources = daofind(image, fwhm=4.0, threshold=3*bkg_sigma)   
+print sources
 
