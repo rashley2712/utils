621e714b6041f0e3a46369f212c5320f1052bdf8
Richard Ashley <rashley@gmail.com>
2014-10-26 22:45:38 +0100

    Testing photutils

--- astropyTest.py
+++ astropyTest.py
@@ -2,6 +2,7 @@
 import astropy 
 import sys
 import photutils
+import matplotlib.pyplot
 import numpy as np
 from photutils import datasets
 
@@ -10,7 +11,10 @@ print "Astropy version:", astropy.__version__
 
 hdu = datasets.load_star_image()   
 image = hdu.data[500:700, 500:700]   
+image = hdu.data
+print np.median(image)
 image -= np.median(image)
+
 
 from photutils import daofind
 from astropy.stats import median_absolute_deviation as mad
@@ -18,3 +22,18 @@ bkg_sigma = 1.48 * mad(image)
 sources = daofind(image, fwhm=4.0, threshold=3*bkg_sigma)   
 print sources
 
+for s in sources:
+	print s
+
+figure = matplotlib.pyplot.figure(figsize=(10, 10))
+matplotlib.pyplot.title("Sample image")
+matplotlib.pyplot.imshow(image, cmap='gray')
+#matplotlib.pyplot.gca().invert_yaxis()	
+matplotlib.pyplot.show()
+
+ax=figure.add_subplot(1,1,1)
+matplotlib.pyplot.axis('off')
+
+extent = ax.get_window_extent().transformed(figure.dpi_scale_trans.inverted())
+
+figure.savefig('test.png', bbox_inches=extent)
