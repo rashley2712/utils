1c5602ce8843c6cfaf60a05196d676e91267d6af
Richard Ashley <rashley@gmail.com>
2015-01-17 14:22:49 +0100

    Included archive functions

--- guardianCrossword.py
+++ guardianCrossword.py
@@ -7,8 +7,42 @@ import sys
 import urllib2
 import os
 
+def getWriteCrossword(fullURL, outputFilename):
+	try:
+		response = urllib2.urlopen(fullURL)
+	except  urllib2.HTTPError as e:
+		print "We got an error of:", e.code
+		sys.exit()
+	except urllib2.URLError as e:
+		print e.reason
+		sys.exit()
+
+	headers = str(response.headers)
+
+	startIndex = headers.find('Content-Type')
+	startIndex+= len("Content-Type: ")
+	endIndex = headers.find('\r\n', startIndex)
+	contentType = headers[startIndex:endIndex]
+
+	if contentType!='application/pdf':
+		print "The server did not return a PDF object."
+		sys.exit()
+
+	pdfData = response.read()
+	print "Fetched the data ok"
+
+	outputFile = open(outputFilename, 'w')
+
+	outputFile.write(pdfData)
+	outputFile.close()
+	
+	print "Written the file to:", outputFilename
+	return
+
 if __name__ == "__main__":
 	days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	
 	testBaseURL = "http://www.devicecloud.co.uk/crosswords/"
 	baseURL = "http://static.guim.co.uk/crosswords/pdfs/"
 	homeDIR = os.getenv("HOME")
@@ -18,13 +52,16 @@ if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description='Downloads the Guardian Quick crosswords and saves (and archives) them to a Dropbox folder.')
 	parser.add_argument('--date', default = 'today', type=str, help='Date for the crossword (default: today)')
+	parser.add_argument('-g', action='store_true', help='\'Get\' directive. Asks the script to get the crossword.')
 	parser.add_argument('--test', action='store_true', help='Use the test URL instead of the real Guardian URL.')
+	parser.add_argument('--archive', action = 'store_true', help='Clean up the Dropbox directory.')
  
 	arg = parser.parse_args()
 	print arg
 	if arg.test:
 		baseURL = testBaseURL
-
+		
+	
 	todaysDate = datetime.datetime.now()
 	requestedDate = todaysDate
 	if arg.date!='today':
@@ -63,41 +100,57 @@ if __name__ == "__main__":
 	dateString = "%d%02d%02d"%(requestedYear, requestedMonth, requestedDay)
 
 	fullURL = baseURL + namePrefix + dateString + nameSuffix
-	print "OK. Now we are ready to fetch: ", fullURL
-
-	try:
-		response = urllib2.urlopen(fullURL)
-	except  urllib2.HTTPError as e:
-		print "We got an error of:", e.code
-		sys.exit()
-	except urllib2.URLError as e:
-		print e.reason
-		sys.exit()
-
-	#for property, value in vars(response).iteritems():
-	#	print property, "| ", value
-
-	headers = str(response.headers)
-	#print "Headers:"
-	#print headers
-
-	startIndex = headers.find('Content-Type')
-	startIndex+= len("Content-Type: ")
-	endIndex = headers.find('\r\n', startIndex)
-	contentType = headers[startIndex:endIndex]
-
-	if contentType!='application/pdf':
-		print "The server did not return a PDF object."
-		sys.exit()
-
-	pdfData = response.read()
-	print "Fetched the data ok"
-
+	print "Ready to fetch: ", fullURL
 	outputFilename = dropboxPath + namePrefix + dateString + nameSuffix
-	outputFile = open(outputFilename, 'w')
 
-	outputFile.write(pdfData)
-
-	outputFile.close()
-
-	print "Written the file to:", outputFilename
+	if (arg.g):
+		getWriteCrossword(fullURL, outputFilename)
+	else:
+		print "You did not specify the 'get' directive, so not really fetching the crossword."
+	
+	if (arg.archive):
+		files = os.listdir(dropboxPath)
+		crosswordFilenames = []
+		dates = []
+		for f in files:
+			if f.find('gdn.quick.')!=-1:
+				crosswordFilenames.append(f)
+				dateString = f[10:18]
+				dates.append(dateString)
+		print "Crosswords found in root folder..."
+		print crosswordFilenames
+		daysOld = []
+		for d in dates:
+			date = datetime.datetime.strptime(d, '%Y%m%d')
+			days = (todaysDate - date).days
+			daysOld.append(days)
+			
+		oldCrosswords = []	
+		oldCrosswordDates = []
+		for index, f in enumerate(crosswordFilenames):
+			if daysOld[index] > 7:
+				oldCrosswords.append(f)
+				oldCrosswordDates.append(dates[index])
+				
+		print "Crosswords older than 7 days..."
+		print oldCrosswords
+		
+		for index, filename in enumerate(oldCrosswords):
+			date = datetime.datetime.strptime(oldCrosswordDates[index], '%Y%m%d')
+			print filename,date
+			month = date.month
+			monthString = months[month-1]
+			year = date.year
+			print year, monthString
+			directory = str(year) + "-" + monthString
+			fullDirectory = dropboxPath + "/" + directory
+			if not os.path.exists(fullDirectory):
+				print "Creating the directory: " + fullDirectory
+				os.mkdir(fullDirectory)
+			oldFilename = dropboxPath + "/" + filename
+			newFilename = dropboxPath + "/" + directory + "/" + filename
+			print oldFilename, newFilename 
+			os.rename(oldFilename, newFilename)
+				
+			
+	print 'Completed successfully'
