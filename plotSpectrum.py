7436a60a0d2cac9ac98d8e5445a473bcf37f2efa
Richard Ashley <rashley@gmail.com>
2015-07-20 08:34:04 +0100

    Adding plotSpectrum

---
+++ plotSpectrum.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import sys
+import numpy, math
+import argparse
+import loadingSavingUtils
+import spectrumClasses, timeClasses
+import scipy.optimize
+import copy
+import ppgplot
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Loads a spectrum JSON file and plots it with PGPLOT.')
+	parser.add_argument('inputFiles', type=str, nargs='+', help='JSON files containing the spectra')
+	parser.add_argument('-e', type=str, help='Optional ephemeris file')
+	parser.add_argument('--list', action='store_true', help='Specify this option if the input file is actually a list of input files.')
+	parser.add_argument('--device', type=str, default = "/xs", help='[Optional] PGPLOT device. Defaults to "/xs".')
+	 
+	arg = parser.parse_args()
+	print arg
+	
+	
+	if arg.e!=None:
+		# Load the ephemeris file
+		hasEphemeris = True
+		ephemeris = timeClasses.ephemerisObject()
+		ephemeris.loadFromFile(arg.e)
+		print ephemeris
+	else:
+		hasEphemeris = False
+
+	
+	filenames = []
+	if arg.list:
+		# Load the list of files.
+		if len(arg.inputFiles)>1: 
+			print "You can only give me one list of filenames."
+			sys.exit()
+		filename = arg.inputFiles[0]
+		fileList = open(filename, 'r')
+		for line in fileList:
+			filenames.append(str(line.strip()))
+	else:
+		filenames = arg.inputFiles
+	
+	
+	spectra = []
+	for fileIndex, f in enumerate(filenames):
+		spectrum = spectrumClasses.spectrumObject()
+		spectrum.loadFromJSON(f)
+		print "Loaded %s, contains %s."%(f, spectrum.objectName)
+		spectra.append(spectrum)
+		
+	numSpectra = len(spectra)
+	if numSpectra>1:
+		print "%d spectra have been loaded."%numSpectra
+		
+	mainPGPlotWindow = ppgplot.pgopen('/xs')	
+	ppgplot.pgask(True)
+	pgPlotTransform = [0, 1, 0, 0, 0, 1]
+	yUpper = 2.5
+	yLower = -0.5
+	
+	newSpectra = []
+	for spectrum in spectra:
+		print spectrum
+		ppgplot.pgsci(1)
+		lowerWavelength = min(spectrum.wavelengths)
+		upperWavelength = max(spectrum.wavelengths)
+		lowerFlux = min(spectrum.flux)
+		upperFlux = max(spectrum.flux)
+		ppgplot.pgenv(lowerWavelength, upperWavelength, yLower, yUpper, 0, 0)
+		ppgplot.pgline(spectrum.wavelengths, spectrum.flux)
+		if hasEphemeris:
+			ppgplot.pglab("wavelength", "flux", "%s [%f]"%(spectrum.objectName, ephemeris.getPhase(spectrum.HJD)))
+		else:
+			ppgplot.pglab("wavelength", "flux", spectrum.objectName)
+	
+		
+	
